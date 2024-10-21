d636343d37444209bb84a2c2b600e1db5bbe8146
Lily Ebadi <lilly888_ebadi@yahoo.com>
2024-10-21 16:31:44 -0500

    simple server.js tested

---
+++ server/utils/seeds.js
@@ -0,0 +1,11 @@
+const connection = require ('..config/db');
+const { Item,User } = require ('../models');
+const { getRandomItem, getRandomUser } = require('./helper');
+
+de.on(error', (err) => err);
+
+db.once('open',async () => {
+    comnsole.log('connected');
+    // Delete the collections if they exist
+    let itemCheck
+}
