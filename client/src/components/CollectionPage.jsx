47293069527561684e6a3a9bf470a2742424c145
Kalen <167487804+Ksjames22@users.noreply.github.com>
2024-10-22 20:49:13 -0400

    changes to collection card

---
+++ client/src/components/CollectionPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CollectionPage = () => {
+    return (
+        <div>
+            <h1>Your Collection</h1>
+            {/* Add your collection items here */}
+        </div>
+    );
+};
+
+export default CollectionPage;
