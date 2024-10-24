a0c81e0cfc790ca39b0f30be094bffc91c6f4565
EbLily <lilly888_ebadi@yahoo.com>
2024-10-24 13:31:28 -0500

    updated seed.js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,33 +1,45 @@
-const db = require('../config/db');
-const { User, Item } = require('../models');
-const userSeeds = require('./userSeeds.json');
-const itemSeeds = require('./itemSeeds.json');
-const cleanDB = require('./cleanDB');
+// server/seeders/seed.js
 
-db.once('open', async () => {
-  try {
-    await cleanDB('Item', 'items');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const User = require('../models/User');
+const Product = require('../models/Product');
 
-    await cleanDB('User', 'users');
+// Load environment variables
+dotenv.config();
 
-    await User.create(userSeeds);
+// import userSeed.json
+const users = require('./userSeed.json');
 
-    for (let i = 0; i < itemSeeds.length; i++) {
-      const { _id, item} = await Item(itemSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: itemUser },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
+
+// load productSeed.json
+const products = require('./productSeed.json');
+
+// Seed Function
+const seedDB = async () => {
+    try {
+        // Connect to MongoDB
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/giftly_db', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB for seeding');
+
+        // Insert Users
+        await User.create(users);
+        console.log('Users inserted');
+
+        // Insert Products
+        await Product.create(products);
+        console.log('Products inserted');
+
+        // Disconnect from MongoDB
+        mongoose.disconnect();
+        console.log('Database seeding completed and disconnected');
+    } catch (error) {
+        console.error('Error seeding the database:', error);
     }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+};
 
-  console.log('all done!');
-  process.exit(0);
-});
+// Execute the seed function
+seedDB();
