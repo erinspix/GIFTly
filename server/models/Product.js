aae8c95546ebba743fee6987277399cbb3ab73d5
EbLily <lilly888_ebadi@yahoo.com>
2024-10-24 13:07:58 -0500

    created Product.js

---
+++ server/models/Product.js
@@ -0,0 +1,48 @@
+// const mongoose = require('mongoose');
+
+// const itemSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   location: { type: String },
+//   price: { type: Number, required: true },
+//   imageUrl: { type: String },
+//   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Link to user
+// });
+
+// const Item = mongoose.model('Item', itemSchema);
+
+// module.exports = Item;
+// Define the Product schema
+const productSchema = new Schema({
+  name: {
+      type: String,
+      required: true,
+      trim: true,
+  },
+  price: {
+      type: Number,
+      required: true,
+  },
+  craftsman: {
+      type: String,
+      required: true,
+      trim: true,
+  },
+  location: {
+      type: String,
+      required: true,
+      trim: true,
+  },
+  imageUrl: {
+      type: String,
+      required: true,
+      default: 'bamboo-serving-tray.png',
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now,
+  },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
