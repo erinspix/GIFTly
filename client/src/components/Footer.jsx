54f73e44a4d1000ecde8998f09af22605e11e352
erinspix <e.spix@yahoo.com>
2024-10-27 16:28:18 -0400

    WORK GIF!

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Text, Image } from '@chakra-ui/react';
-import gifImage from './giftlygif.gif'; // Adjust with the actual file name
+import gifImage from 'giftlygif.gif'; 
 
 const Footer = () => {
     return (
