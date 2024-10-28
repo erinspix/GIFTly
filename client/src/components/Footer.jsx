fb29541d58323ebb270689db3ea7c2be9253b360
erinspix <e.spix@yahoo.com>
2024-10-27 21:35:09 -0400

    gif?

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
                 justifyContent="center"
                 mb={4}
                 height="200px"
-                backgroundImage={`url(${process.env.PUBLIC_URL}/giftlyDarkBlue.gif)`}
+                backgroundImage="url('/giftlyDarkBlue.gif')" // Direct path
                 backgroundRepeat="no-repeat"
                 backgroundPosition="center"
                 backgroundSize="contain"
